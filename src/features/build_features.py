696bbd2ef3a086ae0afc23c3d77f8f61dda3e5f3
jaegglic (Insel) <christoph.jaeggli@insel.ch>
2019-07-15 18:18:14 +0200

    First try to read the excel file

---
+++ src/features/build_features.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Turn the raw data into features for modeling.
+"""
+
+import pandas as pd
+
+
+data_xlsx = pd.read_excel('..\\..\\data\\raw\\GFR_ABL_v_Cobas_1903_1906.xlsx')
+
+
+ahu = 1
+
